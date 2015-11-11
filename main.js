f0398b80cb721d4284ca51c802a82cbf4f05be38
Samuele Resca <samuele.resca@gmail.com>
2015-11-11 21:14:00 +0100

    Tray icon test

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 /* global __dirname */
 /*MODULES INCLUDE*/
 var app = require('app');
+var Menu = require('menu');
+var Tray = require('tray');
 var BrowserWindow = require('browser-window');
 require('crash-reporter').start();
 
 //Init the main window
 var mainWindow = null;
+var trayIcon= null;
+
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -24,4 +28,23 @@ app.on('ready', function () {
   mainWindow.on('closed', function () {
     mainWindow = null;
   });
+  
+  //DEMO TRAY ICON
+  //______________
+  
+  //DEFINES SINGLE LABEL
+  var closeAppLbl=  { label: 'Close application', click:function(){
+    app.quit(); 
+  }};
+  
+  //NEW TRAYICON
+  trayIcon = new Tray('images/imageTray.png');
+  //INIT TRAY ICON MENU
+  var contextMenu = Menu.buildFromTemplate([
+   closeAppLbl
+  ]);
+
+
+  trayIcon.setToolTip('My app');
+  trayIcon.setContextMenu(contextMenu);
 });
