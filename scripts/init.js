3f786e0c664ae3a4fdf9a1e9f3e6e4178a0b516d
Aquino Giorgio <tech584@yoox.com>
2015-12-03 14:18:17 +0100

    Call API for C8 code data

--- scripts/init.js
+++ scripts/init.js
@@ -2,17 +2,41 @@
 require('./scripts/infoLog');
 require('./scripts/fileManager');
 require('./scripts/dialogManager');
+
+var $ = require('jquery');
+
 var fs = require('fs');
 
 //On load
-window.onload = function () {
-  $M.infoLog();
-  $M.fileManager();
-  $M.dialogManager();
-  
-  
-  //DEMO FILE READER
-  fs.readFile('demo-file', 'utf8', function(err,data) {
-    console.log(data);
+// window.onload = function () {
+$(function () {
+  // $M.infoLog();
+  // $M.fileManager();
+  // $M.dialogManager();
+
+  var API_URL = "http://api.yoox.biz/Item.API/1.0/MOSCHINO_IT/item/##c8##.json";
+
+  function getApiUrl(c8) {
+
+   return API_URL.replace('##c8##', c8);
+  }
+
+  $('#submit').click(function () {
+
+    var c8 = $('#c8Fld').val();
+
+    $.getJSON(getApiUrl(c8)).success(function (data) {
+
+      console.log(data);
+
+    });
+
   });
-}
+
+
+
+  // //DEMO FILE READER
+  // fs.readFile('demo-file', 'utf8', function(err,data) {
+  //   console.log(data);
+  // });
+});
